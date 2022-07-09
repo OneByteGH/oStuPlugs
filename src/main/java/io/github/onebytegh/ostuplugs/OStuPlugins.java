94f9dd15cfbecffaf9f8f2e552f6ae2f5f43b952
OneByte <onebytecodes@gmail.com>
2022-07-09 12:54:43 +0530

    Project Creation

---
+++ src/main/java/io/github/onebytegh/ostuplugs/OStuPlugins.java
@@ -0,0 +1,17 @@
+package io.github.onebytegh.ostuplugs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OStuPlugins extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
