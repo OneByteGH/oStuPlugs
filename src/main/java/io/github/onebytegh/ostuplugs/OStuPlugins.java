89dafefa1ec551ac1a37e4b5165e6e13dc77daaf
OneByte <onebytecodes@gmail.com>
2022-07-10 18:50:03 +0530

    Added the ideas and registered the command

--- src/main/java/io/github/onebytegh/ostuplugs/OStuPlugins.java
+++ src/main/java/io/github/onebytegh/ostuplugs/OStuPlugins.java
@@ -1,13 +1,17 @@
 package io.github.onebytegh.ostuplugs;
 
+import io.github.onebytegh.ostuplugs.commands.OStuPluginsCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+
 public final class OStuPlugins extends JavaPlugin {
+    private final HashMap<Integer, Boolean> map = new HashMap<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getCommand("ostuplugs").setExecutor(new OStuPluginsCommand(this));
     }
 
     @Override
---
+++ src/main/java/io/github/onebytegh/ostuplugs/commands/OStuPluginsCommand.java
@@ -0,0 +1,39 @@
+package io.github.onebytegh.ostuplugs.commands;
+
+import io.github.onebytegh.ostuplugs.OStuPlugins;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class OStuPluginsCommand implements CommandExecutor {
+    private final OStuPlugins plugin;
+
+    public OStuPluginsCommand(OStuPlugins oStuPlugins) {
+        this.plugin = oStuPlugins;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length == 0) {
+            sender.sendMessage(
+            """
+            Idea List:
+             1) A mod that tweets your progress
+             2) Everytime you break a block, a villager spawns and says a Sun Tzu quote
+             3) Minecraft but touching the grass kills you
+             4) Minecraft but when you take damage, it rickrolls you
+             5) Minecraft but you cant mine or craft
+             6) Minecraft but if you look at baby villagers, they gang up and kick you in the balls
+             7) Minecraft but you can craft wet water
+             8) Minecraft without minecraft
+             9) Minecraft but make it look like you have friends
+            10) Minecraft but whenever you look at a sheep, it glows and floats
+            11) Minecraft but only dogs (wolfs for the nerds)
+            12) Minecraft but every time you throw an egg, it hatches but into a pig
+            """);
+        } else {
+
+        }
+        return true;
+    }
+}
